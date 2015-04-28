898f4bd22d3d1cbf4d252ba722334d265dabb3dd
Guilherme Carvalho <gui.ifsp11@gmail.com>
2015-04-28 06:41:11 -0300

    Add new models

--- product/models.py
+++ product/models.py
@@ -14,11 +14,14 @@ class Product(models.Model):
     color = models.CharField(max_length=30)
     slug = models.SlugField()
     publishing_date = models.DateTimeField(auto_now_add=True)
-    sku = models.CharField(max_length=50, unique=True)  # unique product identifier
+    sku = models.CharField(max_length=50, unique=True)
     category = models.ForeignKey('Category')
 
     class Meta:
         ordering = ['-publishing_date']
+
+    def __unicode__(self):
+        return '{0}. {1}'.format(self.sku, self.short_description)
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'slug', self.slug})
@@ -32,6 +35,9 @@ class Category(models.Model):
     class Meta:
         ordering = ['name']
 
+    def __unicode__(self):
+        return '{0}'.format(self.name)
+
 
 class ProductDetail(models.Model):
     name = models.CharField(max_length=50)
@@ -39,3 +45,37 @@ class ProductDetail(models.Model):
     product = models.ForeignKey('Product')
 
     ordering = ['name']
+
+    def __unicode__(self):
+        return '{0}.{1}: {2}'.format(
+            self.product.sku,
+            self.product.name,
+            self.name
+            )
+
+
+class ProductReview(models.Model):
+    score = models.IntegerField()
+    pros = models.CharField(max_length=350)
+    cons = models.CharField(max_length=350)
+    opinion = models.TextField()
+    creation_date = models.DateTimeField(auto_now=True)
+    visible = models.BooleanField()
+    product = models.ForeignKey('Product')
+    client = models.ForeignKey('client.Client')
+
+    def __unicode__(self):
+        return '{0}.{1}: {2} - {3}'.format(
+            self.product.sku,
+            self.product.name,
+            self.client.name,
+            self.creation_date
+            )
+
+
+class RelatedProduct(models.Model):
+    product_origin = models.ForeignKey('Product', related_name='origin')
+    related_product = models.ForeignKey('Product', related_name='related')
+
+    def __unicode__(self):
+        return '{0} : {1}'.format(self.product_one.name, self.product_two.name)
