fe45745a118f7bc88669cb7dd1ab933276272ca0
Guilherme Carvalho <gui.ifsp11@gmail.com>
2015-04-28 06:40:14 -0300

    Create promotion app

---
+++ promotion/models.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from django.db import models
+
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=50)
+    begin_date = models.DateField()
+    end_date = models.DateField()
+    percentage_discount = models.FloatField()
+    visible = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class PromotionProduct(models.Model):
+    product = models.ForeignKey('product.Product')
+    promotion = models.ForeignKey('Promotion')
+
+    def __unicode__(self):
+        return '{0} - {1} - {2}%%'.format(
+            self.product.name,
+            self.promotion.name,
+            self.promotion.percentage_discount
+            )
